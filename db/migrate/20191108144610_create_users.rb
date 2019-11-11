f40a9e5b72ed17bb28d9523b806a7d2675751d76
David Fellows <dfellows12@gmail.com>
2019-11-11 12:26:46 -0500

    Changed user attributes, ressed

--- db/migrate/20191108144610_create_users.rb
+++ db/migrate/20191108144610_create_users.rb
@@ -3,8 +3,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.string :gender
-      t.integer :age
+      t.string :phone_number
       
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -39,8 +39,7 @@ ActiveRecord::Schema.define(version: 2019_11_08_144854) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "gender"
-    t.integer "age"
+    t.string "phone_number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
